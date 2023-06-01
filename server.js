e7761373281e9faf31e63749004bec4c1661d968
leslieFhub <134671364+leslieFhub@users.noreply.github.com>
2023-06-01 14:57:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,18 +7,12 @@ import { errorHandler, notFound } from "./Middleware/Errors.js";
 import userRouter from "./Routes/UserRoutes.js";
 import orderRouter from "./Routes/orderRoutes.js";
 import cors from "cors";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
 dotenv.config();
 connectDatabase();
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-// Get __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 // API
 app.use("/api/import", ImportData);
@@ -29,25 +23,9 @@ app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
 
-app.use(express.static(__dirname));
-
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.use((req, res, next) => {
-  res.setHeader("Cache-Control", "no-cache, no-store"); // Disable caching for all responses
-  next();
-});
-
 // ERROR HANDLER
 app.use(notFound);
 app.use(errorHandler);
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
-  next();
-});
 
 const PORT = process.env.PORT || 1000;
 
