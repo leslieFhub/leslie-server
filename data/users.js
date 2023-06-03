3903e3b20c83d05740676b894b0be0326eb2f1f1
leslieFhub <134671364+leslieFhub@users.noreply.github.com>
2023-06-03 14:22:29 +0800

    Update users.js

--- data/users.js
+++ data/users.js
@@ -4,18 +4,21 @@ const users = [
   {
     name: "Admin",
     email: "admin@gmail.com",
+    phone: "639568721584",
     password: bcrypt.hashSync("123456", 10),
     isAdmin: true,
   },
   {
     name: "User",
     email: "user@gmail.com",
+    phone: "639568721584",
     password: bcrypt.hashSync("123456", 10),
     isAdmin: true,
   },
   {
     name: "angeline",
     email: "angeline@gmail.com",
+    phone: "639568721584",
     password: bcrypt.hashSync("LesliesAdminOnly", 10),
     isAdmin: true,
   },
